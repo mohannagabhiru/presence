d398b5aaefd2f65a512fff5c894404893327d0bd
mohanchowdarynr <chiranjeevichowdary7@gmail.com>
2023-04-17 02:07:35 +0530

    Projects Updated

--- src/components/Project.js
+++ src/components/Project.js
@@ -6,11 +6,11 @@ const Project = () => {
         <div className="w-4/5 h-1/2 mx-auto py-4 flex flex-col text-center justify-center align-middle" id="projects">
         <p className="text-3xl font-medium">Projects</p>
         <div className="w-2/3 mx-auto">
+        <ProjectCard name="Cook Fast" link="https://cookfast.netlify.app/" overview="One stop solution to entry level folks for recipes with ingredients to servings"/> 
+        <ProjectCard name="Crypto Tracker" link="https://gotocrypto.netlify.app/" overview="All crypto lovers, this is the site to check where is our portfolio heading to"/> 
         <ProjectCard name="Furniture Store" link="https://furniture-store23.netlify.app/" overview="A mini Store where you can purchase furniture of different brands"/> 
-        <ProjectCard name="Mini Tube" link="https://youtube233.netlify.app/" overview="Mini youtube app where we can select channels,search and view trailer,videos etc.."/> 
         {/* <ProjectCard name="Crypto World" link="https://cryptoworld23.netlify.app/" overview="Crypto currency realted information like exchanges,marketcap,details etc.."/> 
         <ProjectCard name="Storage Drive" link="https://storage-drive.netlify.app/" overview="Similar to google drive where we can store our data "/>  */}
-        <ProjectCard name="Evernote Clone" link="https://evernote-clone23.netlify.app/" overview="Note taking app with crud operations"/> 
         </div>
         </div>
     )
