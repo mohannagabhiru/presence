f8c109d3de834c9c93aaac49b34f92d5aa524d01
mohannagabhiru <chiranjeevichowdary7@gmail.com>
2023-06-04 21:02:51 +0530

    Made changes to about section

--- src/components/About.js
+++ src/components/About.js
@@ -5,9 +5,15 @@ import hero from "../pics/Mohann.jpg";
 const About = () => {
     return (
         <div className="w-4/5 h-1/2 mx-auto bg-white shadow-xl py-4 flex flex-col text-center justify-center align-middle">
-            <img src={hero} alt="profile" className="h-60 w-60 object-cover mt-8 mx-auto rounded-full shadow-2xl"/>
-            <h1 className="mt-10 text-3xl text-blue-500 font-bold">Mohan nagabhiru</h1>
-            <h6 className="mt-10 text-xl  sm:w-3/5 w-4/5 mx-auto opacity-80">As a highly skilled and motivated front-end developer, I have a passion for creating visually stunning web and mobile experiences. My expertise includes proficiency in HTML, CSS and JavaScript, as well as experience with popular front-end frameworks React and React Native . I am also experienced in working with version control systems such as Git. I am always eager to learn new technologies and stay up-to-date</h6>
+            <div className="flex justify-center items-center sm:flex-row">
+                <div className="flex-initial w-1/2 p-5">
+                    <img src={hero} alt="profile" className="h-60 w-60 object-cover mt-8 mx-auto rounded-full shadow-2xl"/>
+                    <h1 className="mt-10 text-3xl text-red-500 font-bold">Mohan Nagabhiru</h1>
+                </div>
+                <div className="flex-initial w-1/2 border-dashed border-2 border-red-500 m-10 p-10">
+                    <h6 className='italic'>As a highly skilled and motivated front-end developer, I have a passion for creating visually stunning web and mobile experiences. My expertise includes proficiency in HTML, CSS and JavaScript, as well as experience with popular front-end frameworks React and React Native . I am also experienced in working with version control systems such as Git. I am always eager to learn new technologies and stay up-to-date</h6>
+                </div>
+            </div>
             <div className="flex justify-center mt-10">
             <a className="text-2xl mr-6 p-2 rounded-full hover:bg-black hover:text-white" href="https://github.com/mohannagabhiru">
             <FaGithub />
